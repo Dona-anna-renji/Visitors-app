fd756b1a090468a9dd92f4740be4e18cecfeff21
Dona-anna-renji <2023annadona@gmail.com>
2024-12-19 11:47:53 +0530

    viewapi completed

--- app/src/main/java/com/example/visitorsapp/MainActivity.java
+++ app/src/main/java/com/example/visitorsapp/MainActivity.java
@@ -30,5 +30,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent o3= new Intent(getApplicationContext(),ViewActivity.class);
+                startActivity(o3);
+            }
+        });
     }
 }
